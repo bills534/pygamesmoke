e6839e6d7c1393d7fea2cac048ce6a9b40563630
Bill <bills534@gmail.com>
2022-03-02 08:05:07 -0500

    added art, particle logic

--- smokedemo.py
+++ smokedemo.py
@@ -10,6 +10,22 @@ screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
 FPS = 60
 
+IMAGE = pygame.image.load('cloudsmall.png')
+
+
+class SmokeParticle:
+    def __init__(self, x=width//2, y=height//2):
+        self.x = x
+        self.y = y
+        self.img = IMAGE
+
+    def update(self):
+        pass
+
+    def draw(self):
+        screen.blit(self.img, self.img.get_rect(center=(self.x, self.y)))
+
+smoke_particle = SmokeParticle()
 
 def main_game():
     while True:
@@ -22,6 +38,8 @@ def main_game():
                     quit()
         
         screen.fill((0,0,0))
+        smoke_particle.update()
+        smoke_particle.draw()
         pygame.display.update()
         clock.tick(FPS)
 
