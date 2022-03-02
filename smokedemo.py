57e1b22d3ed80cde36498479684e6c7c0dcebd8e
Bill <bills534@gmail.com>
2022-03-02 18:45:25 -0500

    puff the magic dragon

--- smokedemo.py
+++ smokedemo.py
@@ -10,17 +10,46 @@ screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
 FPS = 60
 
+
+def scale(img: pygame.Surface, factor):
+    w, h = img.get_width() * factor, img.get_height() * factor
+    return pygame.transform.scale(img, (int(w), int(h)))
+
+
 IMAGE = pygame.image.load('cloudsmall.png')
 
-
 class SmokeParticle:
-    def __init__(self, x=width//2, y=height//2):
+    def __init__(self, x=width//2, y=height//2 + 200):
         self.x = x
         self.y = y
-        self.img = IMAGE
+        self.scale_k = 0.1
+        self.img = scale(IMAGE, self.scale_k)
+        self.alpha = 255
+        self.alpha_decay_rate = 4
+        self.alive = True
+        self.vx = 0
+        self.vy = 4 + (random.randint(7,10) / 10)
+        self.horizontal_velocity = 0.01 * random.random() * random.choice([-1,1])
+        self.vertical_decay = 0.99
 
     def update(self):
-        pass
+        # setting the inital velocity of the particle
+        self.x += self.vx
+        # then adding the horizontal velocity to it
+        self.vx += self.horizontal_velocity
+
+        self.y -= self.vy
+        self.vy *= self.vertical_decay
+
+        self.scale_k += 0.005
+        self.alpha -= self.alpha_decay_rate
+        self.alpha_decay_rate -= 0.1
+        if self.alpha_decay_rate < 1.5:
+            self.alpha_decay_rate = 1.5
+        self.img = scale(IMAGE, self.scale_k)
+        self.img.set_alpha(self.alpha)
+
+        
 
     def draw(self):
         screen.blit(self.img, self.img.get_rect(center=(self.x, self.y)))
